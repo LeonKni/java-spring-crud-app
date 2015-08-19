e032df407e4874597c4540206c02ae9911d26afd
Leon K <leonkni10@gmail.com>
2015-08-19 11:52:30 -0500

    add gradle dependencies

--- src/main/java/com/exercise/unity/controller/SettingsController.java
+++ src/main/java/com/exercise/unity/controller/SettingsController.java
@@ -1,11 +1,16 @@
 package com.exercise.unity.controller;
 
+import com.exercise.unity.model.SettingModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * SettingsController.java.
@@ -31,15 +36,14 @@ public class SettingsController {
      * @return
      */
     @RequestMapping(method = RequestMethod.GET)
-    public String getSettings() throws Exception {
-//        SettingModel model = new SettingModel();
-//        model.setKey("minEnemyCount");
-//        model.setValue("5");
-//        model.setGroupName("difficulty");
-//        Map<String,Object> map = new HashMap<String, Object>();
-//        map.put("retObj",model);
-//        return new ResponseEntity<Object>(model,HttpStatus.OK);
-        return "test";
+    public ResponseEntity<Object> getSettings() throws Exception {
+        SettingModel model = new SettingModel();
+        model.setKey("minEnemyCount");
+        model.setValue("5");
+        model.setGroupName("difficulty");
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("retObj", model);
+        return new ResponseEntity<Object>(model, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
